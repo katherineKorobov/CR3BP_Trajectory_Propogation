bbb80abf246842b6ca53eb709d937b4434098dcf
Katherine Korobov <katya.korobov@gmail.com>
2025-05-28 17:34:48 -0600

    added file for propogation

---
+++ CR3BP.py
@@ -0,0 +1,17 @@
+# #Katherine Korbov
+# Cislunar Initial Orbit Determination Using Adbissible Regions Theory CU SPUR Project
+#Mentor: Queenique Dinh, Marcus Holzinger
+#Date: May 28, 2024
+## This code is a Python script that simulates the motion of a spacecraft in the Circular Restricted Three-Body Problem (CR3BP) using the -- method for numerical integration.
+
+#imports libraries needed, runge kutta integration?
+
+#define function to set constants: orrbit properities 
+
+#set initial conditions and initial condition vector/array
+
+#use integration method and EOM to propagate a new state vector 
+
+#for loop to propagate state vector for a set number of iterations
+
+#plot results
